15781ccad42681f051fa7e0481c7c00c56e4a982
Duncan Malashock <duncanmalashock@gmail.com>
2018-08-17 00:05:40 -0400

    Auto-create 1980 playlist after auth

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,7 +12,11 @@ class Song < ApplicationRecord
     Song.create(title: title, date_first_charted: date_first_charted, artist: a)
   end
 
-  def to_s
-    "#{artist.name}: #{self.title} (#{date_first_charted.strftime("%Y")})"
+  def self.all_from_year(year:)
+    Song.where(:date_first_charted => Date.new(year,1,1).beginning_of_day..Date.new(year,1,1).end_of_day).count
+  end
+
+  def to_query_string
+    "#{artist.name} #{self.title}"
   end
 end
