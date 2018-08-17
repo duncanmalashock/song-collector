369ae580d4f140395063f4504e6b9df8e48d49df
Duncan Malashock <duncanmalashock@gmail.com>
2018-08-17 16:02:33 -0400

    Create playlists after oauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,11 @@ class UsersController < ApplicationController
   def spotify
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
 
-    playlist = @spotify_user.create_playlist!('R&B 1980')
-
-    songs_from_1980 = Song.all_from_year(year: 1980)
-    songs_from_1980.each do |s|
-      search_result = RSpotify::Track.search(s.to_query_string, limit: 1)
-      if search_result != [] then
-        playlist.add_tracks! search_result
+    (1980..1989).map do |year|
+      song_uris = Song.all_from_year(year: year).map { |s| s.spotify_uri }
+      playlist = @spotify_user.create_playlist!("R&B #{year}")
+        playlist.add_tracks! song_uris.compact
       end
-      sleep 2
     end
   end
 end
